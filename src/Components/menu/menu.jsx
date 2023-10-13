caecfe3c38dddc3175a55036b5e69a2c07926a33
BenjiChej <139584822+BenjiChej@users.noreply.github.com>
2023-10-13 19:30:50 -0300

    Update menu.jsx

--- src/Components/menu/menu.jsx
+++ src/Components/menu/menu.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
         <NavBar/>
         <h1 className='descriptit'>¿Porque elegir LATE Chocolates?</h1>
 
-        <h2 className='descrip'>Desde LATE Chocolates hacemos todo con mucho amor y dedicacion al chocolate. Lo que una vez empezo como un hobby hoy se ha convertido en algo mucho mas que ello. La dedicacion una familia a logrado posicionar a LATE como un referente en la experimentacion con sabores nunca antes imaginados, combinaciones nunca antes vistas, chocolates nuevos. Sumate a este aventura y forma parte de esta hermosa familia, no te arrepentiras.</h2>
+        <h2 className='descrip'>Desde LATE Chocolates hacemos todo con mucho amor y dedicacion al chocolate. Lo que una vez empezo como un hobby hoy se ha convertido en algo mucho mas que ello. La dedicacion de una familia a logrado posicionar a LATE como un referente en la experimentacion con sabores nunca antes imaginados, combinaciones nunca antes vistas, chocolates nuevos. Sumate a este aventura y forma parte de esta hermosa familia, no te arrepentiras.</h2>
         <h1 className='descriptit2'>Nuestro Equipo</h1>
 
         <Card/>
